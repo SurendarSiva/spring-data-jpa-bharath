b310d0ca066fb28e1f78f0abe96922e3bb9a571d
SurendarSiva <51986062+SurendarSiva@users.noreply.github.com>
2023-02-16 16:10:08 +0530

    Initial commit

---
+++ datajpasts/productdata/src/test/java/io/suren/jpa/product/Productdata1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.suren.jpa.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Productdata1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
